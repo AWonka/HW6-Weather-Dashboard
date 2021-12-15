e893168efcc8439c7757ba2e37293a85f0e5072b
Austin Wonka <wonkaaustin@gmail.com>
2021-12-15 13:30:49 -0500

    eddited some CSS and added an Id to input field, also started on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+// Search and localStorage variables
+const cityInput = document.getElementById('city-input');
+const searchBtn = document.getElementById('button-addon2');
+const searchHistory = document.getElementById('history');
+// Current weather variables
+const todayWeather = document.getElementById('today-weather');
+const todayPic = document.getElementById('today-pic');
+const cityName = document.getElementById('city-name');
+const todayTemp = document.getElementById('temp');
+const todayHum = document.getElementById('humidity');
+const todayWind = document.getElementById('wind');
+const todayUV = document.getElementById('uv');
+// 5 day forecast variables
+const fivedayForecast = document.getElementById('fiveday-forecast');
+const fivedayTitle = document.getElementById('fiveday-title');
+// API Key
+const apiKey = 'af48d67960dc6c043fc7658bf1e89f7f';
+
+// Make sure to remove d-none from todayWeather(id=today-weather) and fivedayForecast(id=fiveday-forecast)
+
 
