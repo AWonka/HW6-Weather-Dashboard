ca935bf72f495ac1a4173632e9c594fecd78fb20
Austin Wonka <wonkaaustin@gmail.com>
2021-12-15 14:28:48 -0500

    was able to pull lat and lon from city name and store the data from 2nd api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,33 @@ const apiKey = 'af48d67960dc6c043fc7658bf1e89f7f';
 
 // Make sure to remove d-none from todayWeather(id=today-weather) and fivedayForecast(id=fiveday-forecast)
 
+// event listener for search functionality and pass that value off to the function
 
+searchBtn.addEventListener('click', function() {
+    var searchValue = cityInput.value;
+    var cityName = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + apiKey;
+    getLatLong(cityName);
+});
+
+function getLatLong(link) {
+    fetch(link)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.coord.lat)
+            console.log(data.coord.lon)
+
+            var cityLat = data.coord.lat;
+            var cityLon = data.coord.lon;
+
+            var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=hourly,minutely&appid=" + apiKey;
+            getForecast(oneCall);
+        })
+};
+
+function getForecast(link) {
+    fetch(link)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+}
